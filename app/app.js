dbc55fcaa6e0bb76f20b9cdc2ec8e73a7db20b1b
Austen Elswick <chaos9theory@Austens-MacBook-Air.local>
2018-04-05 11:47:50 -0600

    added an array of objects with info in them to app.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
 const arrayOfNums = [1,2,3,4];
+const people = [
+  {
+    name: 'austen',
+    age: 23
+  },
+  {
+    name: 'autumn',
+    age: 29
+  }
+]
 
 const sum = arr => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
-  return sum; 
+  return sum;
 }
